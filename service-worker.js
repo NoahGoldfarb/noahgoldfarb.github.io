8e737855de2470f4b0b5e624bb45bcf7cc16a5aa
NoahGoldfarb <166847260+NoahGoldfarb@users.noreply.github.com>
2026-01-20 13:26:00 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,45 @@
+
+// service-worker.js
+// This file enables offline functionality for your workout app
+
+const CACHE_NAME = 'workout-app-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  'https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js',
+  'https://www.gstatic.com/firebasejs/9.22.0/firebase-database-compat.js'
+];
+
+// Install service worker and cache resources
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Serve cached content when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Clean up old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
